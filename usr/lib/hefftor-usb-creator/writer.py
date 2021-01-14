f32c1636a7e687901d2baf81eec3f3eaec701992
Brad Heffernan <ph3onix83@gmail.com>
2021-01-14 18:08:02 +1030

    updated

--- usr/lib/hefftor-usb-creator/writer.py
+++ usr/lib/hefftor-usb-creator/writer.py
@@ -1,30 +1,65 @@
 #!/usr/bin/python3
 
-import subprocess
-from subprocess import Popen,PIPE,call,STDOUT
-import os, sys
+import os
+import sys
 import getopt
-sys.path.append('/usr/lib/mintstick')
-from mountutils import *
+from subprocess import call
 import parted
+# sys.path.append('/usr/lib/mintstick')
+
+
+def do_umount(target):
+    mounts = get_mounted(target)
+    if mounts:
+        print('Unmounting all partitions of ' + target + ':')
+    for mount in mounts:
+        print('Trying to unmount ' + mount[0] + '...')
+        try:
+            retcode = call('umount ' + mount[0], shell=True)
+            if retcode < 0:
+                print('Error, umount ' + mount[0] +
+                      ' was terminated by signal ' + str(retcode))
+                sys.exit(6)
+            else:
+                if retcode == 0:
+                    print(mount[0] + ' successfully unmounted')
+                else:
+                    print('Error, umount ' + mount[0] +
+                          ' returned ' + str(retcode))
+                    sys.exit(6)
+        except OSError as e:
+            print('Execution failed: ' + str(e))
+            sys.exit(6)
+
+
+def get_mounted(target):
+    try:
+        lines = [line.strip("\n").split(" ") for
+                 line in open("/etc/mtab", "r").readlines()]
+        return [mount for mount in lines if mount[0].startswith(target)]
+    except Exception as e:
+        print('Could not read mtab ! ' + str(e))
+        sys.exit(6)
+
 
 def raw_write(source, target):
     do_umount(target)
     bs = 4096
-    size=0
+    size = 0
     input = open(source, 'rb')
     total_size = float(os.path.getsize(source))
-    #print total_size
+    # print total_size
 
     # Check if the ISO can fit ... :)
     device = parted.getDevice(target)
-    device_size = device.getLength() * device.sectorSize
-    if (device.getLength() * device.sectorSize) < float(os.path.getsize(source)):
+    # device_size = device.getLength() * device.sectorSize
+    if (device.getLength() *
+       device.sectorSize) < float(os.path.getsize(source)):
         input.close()
         print("nospace")
         exit(3)
 
-    increment = total_size / 100;
+    increment = total_size / 100
 
     written = 0
     output = open(target, 'wb')
@@ -47,15 +82,19 @@ def raw_write(source, target):
     output.close()
     if size == total_size:
         print("1.0")
-        exit (0)
+        exit(0)
     else:
         print("failed")
-        exit (4)
+        exit(4)
+
 
 def main():
     # parse command line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hs:t:", ["help", "source=","target="])
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   "hs:t:",
+                                   ["help", "source=",
+                                    "target="])
     except getopt.error as msg:
         print(msg)
         print("for help use --help")
@@ -80,6 +119,7 @@ def main():
         exit(2)
 
     raw_write(source, target)
+
 
 if __name__ == "__main__":
     main()
